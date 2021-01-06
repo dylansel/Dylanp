6b156236ebf3017029d85b454195e1a57834be2e
dylansel <dylansel@users.noreply.github.com>
2021-01-06 18:28:45 -0300

    d

---
+++ apps.js
@@ -0,0 +1,22 @@
+
+
+function verificarContra(){
+    var contraseña = document.getElementById("contraseñaa").value;
+    if (contraseña=="123456"){
+        console.log("La contraseña es Correcta");
+        error.innerHTML = "La contraseña es Correcta"
+        document.getElementById("contenedorSecreto").style.display="block";
+        document.getElementById("error").style.display="block";
+        document.getElementById("error").style.backgroundColor="#2AFF04";
+        document.getElementById("error").style.color="black";
+        document.getElementById("contraseñaa").value = "";
+
+    }else{
+        console.log("La contraseña es Incorrecta");
+        document.getElementById("contraseñaa").value = "";
+        error.innerHTML = "La contraseña es Incorrecta"
+        document.getElementById("error").style.display="block";
+        document.getElementById("contenedorSecreto").style.display="none";
+    }
+    return false;
+}
