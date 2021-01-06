b8ced6a8c21fd4b9730f1ac5e5e05dfd59e6fde1
dylansel <dylansel@users.noreply.github.com>
2021-01-06 20:27:38 -0300

    dd

--- apps.js
+++ apps.js
@@ -1,22 +1,21 @@
 
-
-function verificarContra(){
+function verificarContra() {
     var contraseña = document.getElementById("contraseñaa").value;
-    if (contraseña=="123456"){
+    if (contraseña == "123456") {
         console.log("La contraseña es Correcta");
         error.innerHTML = "La contraseña es Correcta"
-        document.getElementById("contenedorSecreto").style.display="block";
-        document.getElementById("error").style.display="block";
-        document.getElementById("error").style.backgroundColor="#2AFF04";
-        document.getElementById("error").style.color="black";
+        document.getElementById("contenedorSecreto").style.display = "block";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("error").style.backgroundColor = "#2AFF04";
+        document.getElementById("error").style.color = "black";
         document.getElementById("contraseñaa").value = "";
 
-    }else{
+    } else {
         console.log("La contraseña es Incorrecta");
         document.getElementById("contraseñaa").value = "";
         error.innerHTML = "La contraseña es Incorrecta"
-        document.getElementById("error").style.display="block";
-        document.getElementById("contenedorSecreto").style.display="none";
+        document.getElementById("error").style.display = "block";
+        document.getElementById("contenedorSecreto").style.display = "none";
     }
     return false;
 }
